f797c1498f49563037af0e9962c4b2554574d197
yoshihara <yoshihara@users.noreply.github.com>
2017-05-08 13:38:27 +0900

    Implement version command

---
+++ cmd/root.go
@@ -0,0 +1,32 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	// "github.com/spf13/viper"
+)
+
+// RootCmd hoge
+var RootCmd = &cobra.Command{
+	Use:   "kachi",
+	Short: "simple time tracking app",
+	Long:  "simple time tracking app",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(`Please run 'help' command.
+   e.g.) kachi help`)
+	},
+}
+
+func init() {
+	cobra.OnInitialize()
+	RootCmd.AddCommand(versionCmd)
+}
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print the version",
+	Long:  "Print the version",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("1.0.0") // TODO: gitのタグとかを入れる
+	},
+}
