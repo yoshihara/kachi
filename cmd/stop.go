8e8a6689f79eca13c93d124bcf8d84c4e1157654
yoshihara <yoshihara@users.noreply.github.com>
2017-05-11 11:08:47 +0900

    Extract function to complete current Task to lib/

--- cmd/stop.go
+++ cmd/stop.go
@@ -1,12 +1,10 @@
 package cmd
 
 import (
-	"encoding/json"
 	"fmt"
 	"github.com/spf13/cobra"
 	"github.com/yoshihara/kachi/lib"
 	"os"
-	"time"
 )
 
 var stopCmd = &cobra.Command{
@@ -14,33 +12,13 @@ var stopCmd = &cobra.Command{
 	Short: "stop task tracking",
 	Long:  "stop: stop task tracking",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		// TODO: 関数内部はlib/配下に作ってテストを書きたい
-		layout := "Mon Jan 2 15:04:05 MST 2006"
-
 		task, err := lib.ReadCurrentTask()
 		if err != nil {
 			return err
 		}
 
-		task.End = time.Now().Format(layout)
-		taskJSON, err := json.Marshal(task)
-		if err != nil {
-			panic(err)
-		}
+		lib.CompleteTask(task)
 
-		f, err := os.OpenFile("log.json", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
-		if err != nil {
-			panic(err)
-		}
-
-		defer f.Close()
-
-		_, err = f.WriteString(string(taskJSON) + "\n")
-		if err != nil {
-			panic(err)
-		}
-
-		os.Remove("current.json")
 		fmt.Fprintf(os.Stdout, "Task: "+task.Name+" stop\n")
 		return nil
 	},
--- lib/current_task.go
+++ lib/current_task.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"errors"
 	"io/ioutil"
+	"os"
+	"time"
 )
 
 // ReadCurrentTask return the current Task
@@ -21,4 +23,34 @@ func ReadCurrentTask() (*Task, error) {
 	}
 
 	return &task, nil
+}
+
+// CompleteTask add the current Task to log and remove
+func CompleteTask(task *Task) (*Task, error) {
+	layout := "Mon Jan 2 15:04:05 MST 2006"
+
+	task.End = time.Now().Format(layout)
+	taskJSON, err := json.Marshal(task)
+	if err != nil {
+		return nil, err
+	}
+
+	f, err := os.OpenFile("log.json", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		return nil, err
+	}
+
+	defer f.Close()
+
+	_, err = f.WriteString(string(taskJSON) + "\n")
+	if err != nil {
+		return nil, err
+	}
+
+	os.Remove("current.json")
+	if err != nil {
+		return nil, err
+	}
+
+	return task, nil
 }
