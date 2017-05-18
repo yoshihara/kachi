992e3c63764b4822ea1148c735f4502cc940d49a
yoshihara <yoshihara@users.noreply.github.com>
2017-05-18 11:56:39 +0900

    Extract method to show task's stat

--- cmd/stats.go
+++ cmd/stats.go
@@ -1,16 +1,9 @@
 package cmd
 
 import (
-	"encoding/json"
-	"errors"
-	"fmt"
+	"bytes"
 	"github.com/spf13/cobra"
 	"github.com/yoshihara/kachi/lib"
-	"io/ioutil"
-	"math"
-	"os"
-	"strings"
-	"time"
 )
 
 var statsCmd = &cobra.Command{
@@ -18,35 +11,21 @@ var statsCmd = &cobra.Command{
 	Short: "Show tracked tasks",
 	Long:  "stats: Show tracked tasks",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		// TODO: 関数内部はlib/配下に作ってテストを書きたい
-		layout := "Mon Jan 2 15:04:05 MST 2006"
-
-		bytes, error := ioutil.ReadFile("log.json")
+		tasksJSON, error := lib.ReadCompletedTasks()
 		if error != nil {
-			return errors.New("[ERROR] Couldn't read 'log.json'. Please check if 'log.json' exists")
+			return error
 		}
 
-		tasks := string(bytes)
-		task := lib.Task{}
-
-		for _, taskJSON := range strings.Split(tasks, "\n") {
+		for _, taskJSON := range bytes.Split(tasksJSON, []byte("\n")) {
 			if len(taskJSON) < 1 {
 				continue
 			}
-			error = json.Unmarshal([]byte(taskJSON), &task)
+
+			error = lib.ShowTaskStat(taskJSON, scale)
 			if error != nil {
-				return errors.New("[ERROR] Couldn't parse task log as JSON:" + taskJSON)
+				return error
 			}
-			startDateTime, _ := time.Parse(layout, task.Start)
-			endDateTime, _ := time.Parse(layout, task.End)
 
-			// TODO: もうちょっとフォーマット何とかする
-			duration := float64(endDateTime.Sub(startDateTime).Seconds()) * scale
-			minutes := duration / 60.0
-			hours := minutes / 60.0
-
-			// 小数点丸め
-			fmt.Fprintf(os.Stdout, "%s %v\n", task.Name, math.Trunc(hours*100)/100.0)
 		}
 		return nil
 	},
--- lib/completed_tasks.go
+++ lib/completed_tasks.go
@@ -1,9 +1,13 @@
 package lib
 
 import (
+	"encoding/json"
 	"errors"
+	"fmt"
 	"io/ioutil"
+	"math"
 	"os"
+	"time"
 )
 
 // ReadCompletedTasks return completed tasks
@@ -42,5 +46,27 @@ func RefreshTasks() error {
 
 	defer lf.Close()
 
+	return nil
+}
+
+// ShowTaskStat show task stat
+func ShowTaskStat(taskJSON []byte, scale float64) error {
+	task := Task{}
+
+	error := json.Unmarshal([]byte(taskJSON), &task)
+	if error != nil {
+		return errors.New("[ERROR] Couldn't parse task log as JSON:" + string(taskJSON))
+	}
+
+	startDateTime, _ := time.Parse(DateTimeLayout, task.Start)
+	endDateTime, _ := time.Parse(DateTimeLayout, task.End)
+
+	// TODO: もうちょっとフォーマット何とかする
+	duration := float64(endDateTime.Sub(startDateTime).Seconds()) * scale
+	minutes := duration / 60.0
+	hours := minutes / 60.0
+
+	// 小数点丸め
+	fmt.Fprintf(os.Stdout, "%s %v\n", task.Name, math.Trunc(hours*100)/100.0)
 	return nil
 }
